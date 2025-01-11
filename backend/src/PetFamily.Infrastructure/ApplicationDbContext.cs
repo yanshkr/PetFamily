c419cb8b4aad84ee6b15db5907366d4f0cd34ebc
yanshkr <62306961+yanshkr@users.noreply.github.com>
2025-01-11 15:49:08 +0200

    Added connection string.

--- backend/src/PetFamily.Infrastructure/ApplicationDbContext.cs
+++ backend/src/PetFamily.Infrastructure/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace PetFamily.Infrastructure;
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext(IConfiguration configuration) : DbContext
 {
+    private const string DATABASE = "Database";
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString(DATABASE));
         optionsBuilder.UseSnakeCaseNamingConvention();
     }
 }
