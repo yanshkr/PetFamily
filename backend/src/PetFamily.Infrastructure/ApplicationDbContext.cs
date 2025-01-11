0912d604d925799a4e6cfe992548dc7b17ab46cc
yanshkr <62306961+yanshkr@users.noreply.github.com>
2025-01-11 15:46:37 +0200

    Added snake case.

--- backend/src/PetFamily.Infrastructure/ApplicationDbContext.cs
+++ backend/src/PetFamily.Infrastructure/ApplicationDbContext.cs
@@ -3,5 +3,8 @@
 namespace PetFamily.Infrastructure;
 public class ApplicationDbContext : DbContext
 {
-
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSnakeCaseNamingConvention();
+    }
 }
