7d3b615b86d046b67791d4c9a67b7cf194ff04bb
yanshkr <62306961+yanshkr@users.noreply.github.com>
2025-01-21 19:21:24 +0200

    Added and Applied Migrations.

---
+++ backend/src/PetFamily.Domain/Shared/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace PetFamily.Domain.Shared;
+public class Constants
+{
+    public class Shared
+    {
+        public const int NAME_MAX_LENGTH = 100;
+        public const int DESCRIPTION_MAX_LENGTH = 1000;
+    }
+    public class Pet
+    {
+        public const int COLOR_MAX_LENGTH = 50;
+    }
+    public class Volunteer
+    {
+        public const int EMAIL_MAX_LENGTH = 50;
+    }
+}
