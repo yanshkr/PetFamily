e8f7c9824cead640ddaf2d5bf5b73c746ae3256f
yanshkr <62306961+yanshkr@users.noreply.github.com>
2025-01-23 21:16:50 +0200

    Added Repository.

---
+++ backend/src/PetFamily.Application/Features/Volunteers/IVolunteersRepository.cs
@@ -0,0 +1,10 @@
+﻿using CSharpFunctionalExtensions;
+using PetFamily.Domain.Entities;
+using PetFamily.Domain.Shared;
+
+namespace PetFamily.Application.Features.Volunteers;
+public interface IVolunteersRepository
+{
+    Task<Guid> AddAsync(Volunteer volunteer, CancellationToken cancellationToken = default);
+    Task<Result<Volunteer, Error>> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+}
