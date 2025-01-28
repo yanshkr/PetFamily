5de8a0157f27a1ee7aa633056d4c153b09f109b8
yanshkr <62306961+yanshkr@users.noreply.github.com>
2025-01-28 13:36:05 +0200

    Added Global Exception Handler.

--- backend/src/PetFamily.Api/Program.cs
+++ backend/src/PetFamily.Api/Program.cs
@@ -1,11 +1,26 @@
 using PetFamily.Api;
+using PetFamily.Api.Exceptions;
 using PetFamily.Application;
 using PetFamily.Infrastructure;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .MinimumLevel.Debug()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq")!)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .CreateLogger();
+
+builder.Services.AddProblemDetails();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSerilog();
 builder.Services.AddSwaggerGen();
 builder.Services
       .AddInfrastructure()
@@ -14,6 +29,9 @@ builder.Services
 
 var app = builder.Build();
 
+
+app.UseExceptionHandler();
+app.UseSerilogRequestLogging();
 app.UseSwagger();
 app.UseSwaggerUI();
 
