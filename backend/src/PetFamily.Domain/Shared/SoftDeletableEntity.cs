7dd1be7228c872bcab2148f6b03b04de4c79fba6
yanshkr <62306961+yanshkr@users.noreply.github.com>
2025-01-29 11:56:48 +0200

    Added Soft Deletable Entity.

---
+++ backend/src/PetFamily.Domain/Shared/SoftDeletableEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace PetFamily.Domain.Shared;
+public abstract class SoftDeletableEntity<T> : BaseEntity<T> where T : IComparable<T>
+{
+    public bool IsDeleted { get; private set; }
+    public DateTime DeletedAt { get; private set; }
+
+    public virtual void Delete()
+    {
+        IsDeleted = true;
+        DeletedAt = DateTime.UtcNow;
+    }
+
+    public virtual void Restore()
+    {
+        IsDeleted = false;
+        DeletedAt = DateTime.MinValue;
+    }
+}
