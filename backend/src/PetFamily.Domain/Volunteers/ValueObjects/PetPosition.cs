233bfc58570d147baf511ff401a5d48713b00493
yanshkr <62306961+yanshkr@users.noreply.github.com>
2025-01-30 12:48:36 +0200

    Added PetPosition + Configurations.

---
+++ backend/src/PetFamily.Domain/Volunteers/ValueObjects/PetPosition.cs
@@ -0,0 +1,31 @@
+﻿using CSharpFunctionalExtensions;
+using PetFamily.Domain.Shared;
+
+namespace PetFamily.Domain.Volunteers.ValueObjects;
+
+public record PetPosition
+{
+    private const int MIN_VALUE = 1;
+
+    private PetPosition(int value)
+    {
+        Value = value;
+    }
+
+    public int Value { get; }
+
+    public static PetPosition Start => new(MIN_VALUE);
+    public static Result<PetPosition, Error> Create(int value)
+    {
+        if (value < MIN_VALUE)
+            return Errors.General.ValueIsInvalid("SerialNumber");
+
+        return new PetPosition(value);
+    }
+
+    public static bool operator >(PetPosition petPosition1, PetPosition petPosition2) => petPosition1.Value > petPosition2.Value;
+    public static bool operator <(PetPosition petPosition1, PetPosition petPosition2) => petPosition1.Value < petPosition2.Value;
+
+    public static bool operator >=(PetPosition petPosition1, PetPosition petPosition2) => petPosition1.Value >= petPosition2.Value;
+    public static bool operator <=(PetPosition petPosition1, PetPosition petPosition2) => petPosition1.Value <= petPosition2.Value;
+}
--- backend/src/PetFamily.Domain/Volunteers/Volunteer.cs
+++ backend/src/PetFamily.Domain/Volunteers/Volunteer.cs
@@ -89,6 +89,92 @@ public class Volunteer : SoftDeletableEntity<VolunteerId>
     {
         _paymentInfos = paymentInfos.ToList();
     }
+
+    public Result<Pet, Error> GetPetById(PetId petId)
+    {
+        var pet = _pets.FirstOrDefault(p => p.Id == petId);
+        if (pet == null)
+            return Errors.General.NotFound(petId);
+
+        return pet;
+    }
+    public UnitResult<Error> AddPet(Pet pet)
+    {
+        if (_pets.Contains(pet))
+            return Errors.General.AlreadyExists(pet.Id);
+
+        var petPosition = PetPosition.Create(_pets.Count + 1);
+        if (petPosition.IsFailure)
+            return petPosition.Error;
+
+        pet.SetPetPosition(petPosition.Value);
+
+        _pets.Add(pet);
+
+        return UnitResult.Success<Error>();
+    }
+    public UnitResult<Error> RemovePet(Pet pet)
+    {
+        if (!_pets.Contains(pet))
+            return Errors.General.NotFound(pet.Id);
+
+        _pets.Remove(pet);
+
+        foreach (var leastPet in _pets.Where(p => p.PetPosition > pet.PetPosition))
+        {
+            var newPetPosition = PetPosition.Create(leastPet.PetPosition.Value - 1);
+            if (newPetPosition.IsFailure)
+                return newPetPosition.Error;
+
+            leastPet.SetPetPosition(newPetPosition.Value);
+        }
+
+        return UnitResult.Success<Error>();
+    }
+    public UnitResult<Error> MovePet(Pet pet, PetPosition petPosition)
+    {
+        if (!_pets.Contains(pet))
+            return Errors.General.NotFound(pet.Id);
+
+        if (_pets.Count < petPosition.Value)
+            return Errors.General.ValueIsInvalid("PetPosition");
+
+        int direction = pet.PetPosition > petPosition ? 1 :
+                        pet.PetPosition < petPosition ? -1 : 0;
+
+        if (direction == 0)
+            return UnitResult.Success<Error>();
+
+        foreach (var petToMove in _pets.Where(p => p != pet))
+        {
+            if ((direction > 0 && petToMove.PetPosition >= petPosition && petToMove.PetPosition < pet.PetPosition) ||
+                (direction < 0 && petToMove.PetPosition <= petPosition && petToMove.PetPosition > pet.PetPosition))
+            {
+                var newPetPosition = PetPosition.Create(petToMove.PetPosition.Value + direction);
+                if (newPetPosition.IsFailure)
+                    return newPetPosition.Error;
+
+                petToMove.SetPetPosition(newPetPosition.Value);
+            }
+        }
+
+        pet.SetPetPosition(petPosition);
+
+        return UnitResult.Success<Error>();
+    }
+    public UnitResult<Error> MovePetToStart(Pet pet)
+    {
+        return MovePet(pet, PetPosition.Start);
+    }
+    public UnitResult<Error> MovePetToEnd(Pet pet)
+    {
+        var newPetPosition = PetPosition.Create(_pets.Count);
+        if (newPetPosition.IsFailure)
+            return newPetPosition.Error;
+
+        return MovePet(pet, newPetPosition.Value);
+    }
+
     public new void Delete()
     {
         base.Delete();
