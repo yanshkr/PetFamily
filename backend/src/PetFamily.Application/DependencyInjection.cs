79c795a9229d062cacdc99657f6fa6a8ae86cff6
yanshkr <62306961+yanshkr@users.noreply.github.com>
2025-02-18 20:14:58 +0200

    Added Structor.

--- backend/src/PetFamily.Application/DependencyInjection.cs
+++ backend/src/PetFamily.Application/DependencyInjection.cs
@@ -1,39 +1,34 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
-using PetFamily.Application.Features.Species.AddBreed;
-using PetFamily.Application.Features.Species.Create;
-using PetFamily.Application.Features.Species.Delete;
-using PetFamily.Application.Features.Volunteers.AddPet;
-using PetFamily.Application.Features.Volunteers.CreateVolunteer;
-using PetFamily.Application.Features.Volunteers.DeletePhotosAtPet;
-using PetFamily.Application.Features.Volunteers.DeleteVolunteer;
-using PetFamily.Application.Features.Volunteers.UpdatePetPosition;
-using PetFamily.Application.Features.Volunteers.UpdateVolunteerMainInfo;
-using PetFamily.Application.Features.Volunteers.UpdateVolunteerPaymentInfo;
-using PetFamily.Application.Features.Volunteers.UpdateVolunteerSocialMedia;
-using PetFamily.Application.Features.Volunteers.UploadPhotosToPet;
+using PetFamily.Application.Abstraction;
 
 namespace PetFamily.Application;
 public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
-
-        services.AddScoped<CreateVolunteerHandler>();
-        services.AddScoped<UpdateVolunteerMainInfoHandler>();
-        services.AddScoped<UpdateVolunteerSocialMediaHandler>();
-        services.AddScoped<UpdateVolunteerPaymentInfoHandler>();
-        services.AddScoped<DeleteVolunteerHandler>();
-        services.AddScoped<AddPetHandler>();
-        services.AddScoped<UploadPetPhotosHandler>();
-        services.AddScoped<DeletePetPhotosHandler>();
-        services.AddScoped<UpdatePetPositionHandler>();
-
-        services.AddScoped<CreateSpecieHandler>();
-        services.AddScoped<AddBreedHandler>();
-        services.AddScoped<DeleteSpecieHandler>();
+        services
+            .AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly)
+            .AddCommands()
+            .AddQueries();
 
         return services;
+    }
+    private static IServiceCollection AddCommands(this IServiceCollection services)
+    {
+        return services.Scan(scan => scan.FromAssemblies(typeof(DependencyInjection).Assembly)
+            .AddClasses(classes => classes
+                .AssignableToAny(typeof(ICommandHandler<,>), typeof(ICommandHandler<>)))
+            .AsSelfWithInterfaces()
+            .WithScopedLifetime());
+    }
+
+    private static IServiceCollection AddQueries(this IServiceCollection services)
+    {
+        return services.Scan(scan => scan.FromAssemblies(typeof(DependencyInjection).Assembly)
+            .AddClasses(classes => classes
+                .AssignableTo(typeof(IQueryHandler<,>)))
+            .AsSelfWithInterfaces()
+            .WithScopedLifetime());
     }
 }
