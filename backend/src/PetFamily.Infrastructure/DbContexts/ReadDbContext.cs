d6ecee110a0924673f0c208b8ecab523fd517382
yanshkr <62306961+yanshkr@users.noreply.github.com>
2025-02-18 20:13:38 +0200

    Add Read Context.

---
+++ backend/src/PetFamily.Infrastructure/DbContexts/ReadDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetFamily.Application.Database;
+using PetFamily.Application.Dtos;
+
+namespace PetFamily.Infrastructure.DbContexts;
+internal class ReadDbContext(DbContextOptions<ReadDbContext> options) : DbContext(options), IReadDbContext
+{
+    public IQueryable<VolunteerDto> Volunteers => Set<VolunteerDto>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(WriteDbContext).Assembly,
+            type => type.FullName?.Contains("Configurations.Read") ?? false);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
