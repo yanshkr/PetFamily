26b3aac47bbea28a9ccaf96c46b27bbf58ad967e
yanshkr <62306961+yanshkr@users.noreply.github.com>
2025-02-18 20:14:43 +0200

    Added Queries.

---
+++ backend/src/PetFamily.Application/Features/Queries/GetAllVolunteersWithPagination/GetAllVolunteersWithPaginationHandler.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using PetFamily.Application.Abstraction;
+using PetFamily.Application.Database;
+using PetFamily.Application.Dtos;
+using PetFamily.Application.Extensions;
+using PetFamily.Application.Features.Commands.Volunteers.AddPet;
+using PetFamily.Application.Models;
+
+namespace PetFamily.Application.Features.Queries.GetAllVolunteersWithPagination;
+public class GetAllVolunteersWithPaginationHandler 
+    : IQueryHandler<PagedList<VolunteerDto>, GetAllVolunteersWithPaginationQuery>
+{
+    private readonly IReadDbContext _readDbContext;
+    private readonly ILogger<AddPetHandler> _logger;
+    public GetAllVolunteersWithPaginationHandler(
+        IReadDbContext readDbContext,
+        ILogger<AddPetHandler> logger)
+    {
+        _readDbContext = readDbContext;
+        _logger = logger;
+    }
+
+    public async Task<PagedList<VolunteerDto>> HandleAsync(
+        GetAllVolunteersWithPaginationQuery query,
+        CancellationToken cancellationToken = default)
+    {
+        var volunteersQuery = _readDbContext.Volunteers;
+
+        volunteersQuery = volunteersQuery.WhereIf(
+                !string.IsNullOrWhiteSpace(query.FisrtName),
+                x => x.FirstName.Contains(query.FisrtName!));
+
+        volunteersQuery = volunteersQuery.WhereIf(
+                !string.IsNullOrWhiteSpace(query.LastName),
+                x => x.LastName.Contains(query.LastName!));
+
+        volunteersQuery = volunteersQuery.WhereIf(
+                !string.IsNullOrWhiteSpace(query.Email),
+                x => x.Email.Contains(query.Email!));
+
+        volunteersQuery = volunteersQuery.WhereIf(
+                !string.IsNullOrWhiteSpace(query.PhoneNumber),
+                x => x.PhoneNumber.Contains(query.PhoneNumber!));
+
+        return await volunteersQuery
+            .ToPagedList(query.Page, query.PageSize, cancellationToken);
+    }
+}
---
+++ backend/src/PetFamily.Application/Features/Queries/GetVolunteerById/GetVolunteerByIdHandler.cs
@@ -0,0 +1,37 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PetFamily.Application.Abstraction;
+using PetFamily.Application.Database;
+using PetFamily.Application.Dtos;
+using PetFamily.Application.Features.Commands.Volunteers.AddPet;
+using PetFamily.Domain.Shared;
+
+namespace PetFamily.Application.Features.Queries.GetVolunteerById;
+public class GetVolunteerByIdHandler 
+    : IQueryHandler<Result<VolunteerDto, ErrorList>, GetVolunteerByIdQuery>
+{
+    private readonly IReadDbContext _readDbContext;
+    private readonly ILogger<AddPetHandler> _logger;
+    public GetVolunteerByIdHandler(
+        IReadDbContext readDbContext,
+        ILogger<AddPetHandler> logger)
+    {
+        _readDbContext = readDbContext;
+        _logger = logger;
+    }
+
+    public async Task<Result<VolunteerDto, ErrorList>> HandleAsync(
+        GetVolunteerByIdQuery query,
+        CancellationToken cancellationToken = default)
+    {
+
+        var volunteer = await _readDbContext.Volunteers
+            .FirstOrDefaultAsync(v => v.Id == query.Id, cancellationToken);
+
+        if (volunteer is null)
+            return Errors.General.NotFound(query.Id).ToErrorList();
+
+        return volunteer;
+    }
+}
